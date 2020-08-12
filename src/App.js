8286b2dc2c8ac8948299675169f980595486cf27
Maharramoff <sh.maharramov@gmail.com>
2020-08-12 23:41:48 +0400

    chore: move theme to settings folder

--- src/App.js
+++ src/App.js
@@ -2,21 +2,7 @@ import React from 'react';
 import AppTopBar from './components/AppTopBar'
 import { AppProvider } from '@shopify/polaris'
 import enTranslations from '@shopify/polaris/locales/en.json';
-
-const theme = {
-    colors: {
-        topBar: {
-            background: '#357997',
-        },
-    },
-    logo: {
-        width: 124,
-        topBarSource:
-          'https://cdn.shopify.com/s/files/1/0446/6937/files/jaded-pixel-logo-color.svg?6215648040070010999',
-        url: 'http://jadedpixel.com',
-        accessibilityLabel: 'Jaded Pixel',
-    },
-};
+import theme from './settings/theme'
 
 function App()
 {
