230d62b0c11ff326ee89283b3ec3c5cdcd17654c
Maharramoff <sh.maharramov@gmail.com>
2020-08-13 15:03:20 +0400

    chore: update nav logo

--- src/settings/theme.js
+++ src/settings/theme.js
@@ -5,10 +5,10 @@ export default {
         },
     },
     logo: {
-        width: 124,
+        width: 40,
         topBarSource:
-          'https://cdn.shopify.com/s/files/1/0446/6937/files/jaded-pixel-logo-color.svg?6215648040070010999',
-        url: 'http://jadedpixel.com',
-        accessibilityLabel: 'Jaded Pixel',
+          '/logo.svg',
+        url: '/',
+        accessibilityLabel: 'Notes App',
     },
 };
